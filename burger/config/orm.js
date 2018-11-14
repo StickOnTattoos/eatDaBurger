35958fcf88b021a768a28867f056eed44b93e1a7
Tyler <tylerbumguardner@gmail.com>
2018-11-13 20:05:57 -0700

    more stuff

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -1,10 +1,43 @@
 var connection = require("./connection.js");
 
-var orm = {
-    selectAll: function(whatToEat, Eaten) {
-        var queryString = "SELECT * FROM burgers";
+function printQuestionMarks(num) {
+    var arr = [];
 
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
     }
+    
+    return arr.toString();
+};
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    
+    return arr.toString();
+};
+
+var orm = {
+    selectAll: function(whatToEat, cb) {
+        var queryString = "SELECT * FROM " + whatToEat + ";";
+
+        connection.query(queryString, function(err, result) {
+            cb(result);
+        })
+
+    },
+
+    add
 }
 
 module.exports = orm;
